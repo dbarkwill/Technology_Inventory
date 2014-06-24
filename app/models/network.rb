20f2f1c2efb6d4509a16eb59f7b115e0f99e03c5
dbarkwill <dbarkwill@gmail.com>
2014-06-23 20:38:43 -0400

    Fixed network creation issue, minor visual and backend enhancements

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,4 +1,14 @@
 class Network < ActiveRecord::Base
 	has_many :addresses
 	has_many :devices, :through => :addresses
+
+	validate :valid_ip
+
+
+
+	def valid_ip
+		if !(IPAddress(self.network) rescue nil)
+			errors.add(:network, "The provided network is not valid. A valid format is 192.168.0.1/24")
+		end
+	end
 end
