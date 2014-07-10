e55de25d7b0e33075d2c1d2e1266740449e5e086
dbarkwill <dbarkwill@gmail.com>
2014-07-09 22:47:57 -0400

    Fixed editing of profiles and started authorization with Pundit.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   as :user do
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'registration'
   end
 
   resources :people
